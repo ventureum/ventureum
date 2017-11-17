44e49df9df3a53a31910a74d4edea615399ae65d
Timothy Wang <timothywangdev@gmail.com>
2017-11-17 01:41:15 +0000

    Crowdsale contract

---
+++ solidity/contracts/Crowdsale.sol
@@ -0,0 +1,99 @@
+pragma solidity ^0.4.11;
+
+import 'VTHToken.sol';
+import 'zeppelin-solidity/blob/master/contracts/math/SafeMath.sol';
+
+contract Crowdsale {
+  using SafeMath for uint256;
+
+  // The token being sold
+
+  VTHToken public token;
+
+  // start and end timestamps where investments are allowed (both inclusive)
+  uint256 public startTime;
+  uint256 public endTime;
+
+  // address where funds are collected
+  address public wallet;
+
+  // how many token units a buyer gets per wei
+  // pre-sale: 1e4 mVTH / wei or 0.0001 ETH / VTH (1 VTH = 1e18 mVTH)
+  uint256 public rate;
+
+  // amount of raised money in wei
+  uint256 public weiRaised;
+
+  /**
+   * event for token purchase logging
+   * @param purchaser who paid for the tokens
+   * @param beneficiary who got the tokens
+   * @param value weis paid for purchase
+   * @param amount amount of tokens purchased
+   */
+  event TokenPurchase(address indexed purchaser, address indexed beneficiary, uint256 value, uint256 amount);
+
+
+  function Crowdsale(address _token, uint256 _startTime, uint256 _endTime, uint256 _rate, address _wallet) {
+    require(_startTime >= now);
+    require(_endTime >= _startTime);
+    require(_rate > 0);
+    require(_wallet != address(0));
+
+    token = VTHToken(_token);
+    startTime = _startTime;
+    endTime = _endTime;
+    rate = _rate;
+    wallet = _wallet;
+  }
+
+
+  // fallback function can be used to buy tokens
+  function () payable {
+    buyTokens(msg.sender);
+  }
+
+  // low level token purchase function
+  function buyTokens(address beneficiary) public payable {
+    require(beneficiary != address(0));
+    require(validPurchase());
+
+    uint256 weiAmount = msg.value;
+
+    // calculate token amount to be created
+    uint256 tokens = weiAmount.mul(rate);
+
+    // update state
+    weiRaised = weiRaised.add(weiAmount);
+
+    if(!token.transfer(beneficiary, tokens)) {
+      // cannot allocate tokens to the beneficiary, revert changes
+      throw;
+    }
+
+    TokenPurchase(msg.sender, beneficiary, weiAmount, tokens);
+
+    forwardFunds();
+  }
+
+  // send ether to the fund collection wallet
+  // override to create custom fund forwarding mechanisms
+  function forwardFunds() internal {
+    wallet.transfer(msg.value);
+  }
+
+  // @return true if the transaction can buy tokens
+  function validPurchase() internal constant returns (bool) {
+    bool withinPeriod = now >= startTime && now <= endTime;
+    bool nonZeroPurchase = msg.value != 0;
+    bool withinCap = weiRaised.add(msg.value) <= cap;
+    return withinPeriod && nonZeroPurchase && withinCap;
+  }
+
+  // @return true if crowdsale event has ended
+  function hasEnded() public constant returns (bool) {
+    // make sure cap is not reached
+    bool capReached = weiRaised >= cap;
+    return now > endTime || capReached;
+  }
+}
