3ed87530bad2caaa739dd7463c0f54018cd4fb3b
Timothy Wang <timothywangdev@gmail.com>
2017-11-29 04:06:42 +0000

    Fix VGTH supply error

--- solidity/contracts/VGTHToken.sol
+++ solidity/contracts/VGTHToken.sol
@@ -6,12 +6,12 @@ contract VGTHToken is RedeemableERC20Token {
 
   /**
    * construct VGTH token
-   * Total Supply: 15% of VTH supply = 1.5e7 VGTH, or 1.5e25 mVGTH (1 mVGTH = 1e-18 VGTH)
-   * 1.5e7 VGTH tokens are automatically issued to the contract creator
+   * Total Supply: 15% of VTH supply = 1.5e8 VGTH, or 1.5e26 mVGTH (1 mVGTH = 1e-18 VGTH)
+   * 1.5e8 VGTH tokens are automatically issued to the contract creator
    */
   function VGTHToken() RedeemableERC20Token('Ventureum Genesis Token',
                                             'VGTH',
                                             18,
-                                            1.5e25) public {
+                                            1.5e26) public {
   }
 }
