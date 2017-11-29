61deebc4e25a94849e447ad96b2d24c079b54d6e
Timothy Wang <timothywangdev@gmail.com>
2017-11-29 04:04:33 +0000

    VTH Class A and Class B tokens

---
+++ solidity/contracts/VTHTokenClassA.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.4.17;
+
+import './DualClassRedeemableERC20Token.sol';
+
+contract VTHTokenClassA is DualClassRedeemableERC20Token {
+
+  /**
+   * construct VTH Class A token
+   * Totaly Supply: 70% of VTH supply = 7e8 VTH, or 7e26 mVGTH (1 mVTH = 1e-18 VTH)
+   */
+  function VTHTokenClassA() DualClassRedeemableERC20Token('Ventureum Token Class A',
+                                                                          'VTH',
+                                                                          18,
+                                                                          7e26,
+                                                                          true) public {
+
+  }
+}
