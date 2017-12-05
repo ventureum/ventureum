bc7f60d43d7d77ea901203917b7e6be7c098ff01
Timothy Wang <timothywangdev@gmail.com>
2017-12-05 08:02:00 +0000

    Add state definition as a base contract

---
+++ solidity/design/States.sol
@@ -0,0 +1,11 @@
+Contract States {
+  // states
+  uint8 constant INACTIVE = 0;
+  uint8 constant IP = 1;
+  uint8 constant VP1 = 2;
+  uint8 constant VP2 = 3;
+  uint8 constant VP1_AFTER_VP2 = 4;
+  uint8 constant C = 5;
+  uint8 constant RP = 6;
+  uint8 constant WVP2 = 7;
+}
