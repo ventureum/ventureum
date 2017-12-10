87cd59f008e5ba3aba4adbc4fbc028255ccba90c
Timothy Wang <timothywangdev@gmail.com>
2017-12-10 05:39:02 +0000

    Fix indentation.

--- solidity/design/VTHManager.sol
+++ solidity/design/VTHManager.sol
@@ -1,92 +1,92 @@
 Contract VTHManager is Ownable, States {
 
-  // Ventureum Contract
-  Ventureum public ven;
+    // Ventureum Contract
+    Ventureum public ven;
 
-  // project meta info
-  ProjectMeta public projectMeta;
+    // project meta info
+    ProjectMeta public projectMeta;
 
-  // number of VTH tokens staked
-  mapping(address => uint) staked;
+    // number of VTH tokens staked
+    mapping(address => uint) staked;
 
-  struct RC {
-    // RC of the subtree starting at this milestone
-    uint subtree;
-    // RC of this milestone
-    uint vertex;
-  }
-
-  // milestone => investor address => refund coverage
-  mapping(address => mapping(address => RC)) RCByMilestone;
-
-  function stakeVTH(address milestoneAddr, uint value) returns (uint) {
-
-    /** In order to transfer tokens owned by someone else, we need to do it in
-     * two steps:
-     * 1. Investor call token.approve(milestoneAddr, value) from web3
-     * 2. Transfer funds from the investor to this contract using transferFrom()
-     */
-    ERC20 token = ERC20(projectMeta.getVTHAddr());
-    require(token.transferFrom(msg.sender, address(this), value));
-
-    // must be a valid milestone address
-    require(projectMeta.isMilestone(milestoneAddr));
-    Milestone milestone = Milestone(milestoneAddr);
-
-    // must be at in a valid state
-    require(milestone.state() == IP);
-
-    // update number of VTH staked by this address
-    staked[msg.sender] += value;
-
-    // calculate refund coverage
-    uint RCInWei = ven.mVTHToWei(value);
-
-    RCByMilestone[milestoneAddr][msg.sender].vertex += RCInWei;
-    RCByMilestone[milestoneAddr][msg.sender].subtree += RCInWei;
-
-    // update all ancestors
-    while(milestone.parent() != address(0x0)) {
-        milestone = milestone.parent();
-        RCByMilestone[milestone][msg.sender].subtree += RCInWei;
+    struct RC {
+        // RC of the subtree starting at this milestone
+        uint subtree;
+        // RC of this milestone
+        uint vertex;
     }
 
-    return RCInWei;
-  }
+    // milestone => investor address => refund coverage
+    mapping(address => mapping(address => RC)) RCByMilestone;
 
-  function withdrawVTH() returns (bool) {
-    // project must have been completed
-    require(projectMeta.completed());
+    function stakeVTH(address milestoneAddr, uint value) returns (uint) {
 
-    // right now, continuous withdraw is not supported
-    ERC20 vth = ERC20(ven.getVTHAddr());
-    _stakedVTH = staked[msg.sender];
-    staked[msg.sender] = 0;
-    require(vth.transfer(msg.sender, _stakedVTH));
+        /** In order to transfer tokens owned by someone else, we need to do it in
+         * two steps:
+         * 1. Investor call token.approve(milestoneAddr, value) from web3
+         * 2. Transfer funds from the investor to this contract using transferFrom()
+         */
+        ERC20 token = ERC20(projectMeta.getVTHAddr());
+        require(token.transferFrom(msg.sender, address(this), value));
 
-    return true;
-  }
+        // must be a valid milestone address
+        require(projectMeta.isMilestone(milestoneAddr));
+        Milestone milestone = Milestone(milestoneAddr);
 
-  // transfer remaining RC of a milestone to another milestone
-  function transferRC(address from, address to, uint value) returns (bool) {
+        // must be at in a valid state
+        require(milestone.state() == IP);
 
-    // must be a valid milestone address
-    require(projectMeta.isMilestone(from));
-    require(projectMeta.isMilestone(to));
+        // update number of VTH staked by this address
+        staked[msg.sender] += value;
 
-    Milestone milestoneFrom = Milestone(from);
-    Milestone milestoneTo = Milestone(to);
+        // calculate refund coverage
+        uint RCInWei = ven.mVTHToWei(value);
 
-    // both must be in valid states
-    require(milestoneFrom.state() == IP);
-    require(milestoneTo.state() == IP);
+        RCByMilestone[milestoneAddr][msg.sender].vertex += RCInWei;
+        RCByMilestone[milestoneAddr][msg.sender].subtree += RCInWei;
 
-    // now transfer RC
-    require(value <= RCByMilestone[from][msg.sender]);
+        // update all ancestors
+        while(milestone.parent() != address(0x0)) {
+            milestone = milestone.parent();
+            RCByMilestone[milestone][msg.sender].subtree += RCInWei;
+        }
 
-    RCByMilestone[from][msg.sender] -= value;
-    RCByMilestone[to][msg.sender] += value;
+        return RCInWei;
+    }
 
-    return true;
-  }
+    function withdrawVTH() returns (bool) {
+        // project must have been completed
+        require(projectMeta.completed());
+
+        // right now, continuous withdraw is not supported
+        ERC20 vth = ERC20(ven.getVTHAddr());
+        _stakedVTH = staked[msg.sender];
+        staked[msg.sender] = 0;
+        require(vth.transfer(msg.sender, _stakedVTH));
+
+        return true;
+    }
+
+    // transfer remaining RC of a milestone to another milestone
+    function transferRC(address from, address to, uint value) returns (bool) {
+
+        // must be a valid milestone address
+        require(projectMeta.isMilestone(from));
+        require(projectMeta.isMilestone(to));
+
+        Milestone milestoneFrom = Milestone(from);
+        Milestone milestoneTo = Milestone(to);
+
+        // both must be in valid states
+        require(milestoneFrom.state() == IP);
+        require(milestoneTo.state() == IP);
+
+        // now transfer RC
+        require(value <= RCByMilestone[from][msg.sender]);
+
+        RCByMilestone[from][msg.sender] -= value;
+        RCByMilestone[to][msg.sender] += value;
+
+        return true;
+    }
 }
