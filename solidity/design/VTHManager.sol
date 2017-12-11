5ee6487d5ee676c6481a0934d2afd79aef3d8b8b
nathanliufd <nathan_liu_fd@hotmail.com>
2017-12-10 20:48:43 -0500

    Update VTHManager with withdrawVTHByVentureum() and transferVTH()

--- solidity/design/VTHManager.sol
+++ solidity/design/VTHManager.sol
@@ -6,26 +6,28 @@ Contract VTHManager is Ownable, States {
     // project meta info
     ProjectMeta public projectMeta;
 
+    // Ventureum contract, this must be set in the constructor VTHManager()
+    Ventureum public _Ventureum;
+
     // number of VTH tokens staked
     mapping(address => uint) staked;
 
-    struct RC {
-        // RC of the subtree starting at this milestone
-        uint subtree;
-        // RC of this milestone
-        uint vertex;
-    }
-
     // milestone => investor address => refund coverage
-    mapping(address => mapping(address => RC)) RCByMilestone;
+    mapping(address => mapping(address => uint)) RCByMilestone;
+
+    // VTH collected by the Ventureum team as fees
+    unit public VTHOwnedByVentureum;
+
 
     function stakeVTH(address milestoneAddr, uint value) returns (uint) {
 
+        require( msg.sender != address(0x0) );  
+
         /** In order to transfer tokens owned by someone else, we need to do it in
-         * two steps:
-         * 1. Investor call token.approve(milestoneAddr, value) from web3
-         * 2. Transfer funds from the investor to this contract using transferFrom()
-         */
+          * two steps:
+          * 1. Investor call token.approve(milestoneAddr, value) from web3
+          * 2. Transfer funds from the investor to this contract using transferFrom()
+          */
         ERC20 token = ERC20(projectMeta.getVTHAddr());
         require(token.transferFrom(msg.sender, address(this), value));
 
@@ -37,24 +39,22 @@ Contract VTHManager is Ownable, States {
         require(milestone.state() == IP);
 
         // update number of VTH staked by this address
-        staked[msg.sender] += value;
+        staked[msg.sender] += value*0.99;
+
+        // update number of VTH collected by the Ventureum team as fees
+        VTHOwnedByVentureum += value*0.01;
 
         // calculate refund coverage
         uint RCInWei = ven.mVTHToWei(value);
 
-        RCByMilestone[milestoneAddr][msg.sender].vertex += RCInWei;
-        RCByMilestone[milestoneAddr][msg.sender].subtree += RCInWei;
-
-        // update all ancestors
-        while(milestone.parent() != address(0x0)) {
-            milestone = milestone.parent();
-            RCByMilestone[milestone][msg.sender].subtree += RCInWei;
-        }
-
+        RCByMilestone[milestoneAddr][msg.sender] += RCInWei;
         return RCInWei;
     }
 
     function withdrawVTH() returns (bool) {
+
+        require( msg.sender != address(0x0) );
+
         // project must have been completed
         require(projectMeta.completed());
 
@@ -69,6 +69,8 @@ Contract VTHManager is Ownable, States {
 
     // transfer remaining RC of a milestone to another milestone
     function transferRC(address from, address to, uint value) returns (bool) {
+
+        require( msg.sender != address(0x0) );  
 
         // must be a valid milestone address
         require(projectMeta.isMilestone(from));
@@ -89,4 +91,57 @@ Contract VTHManager is Ownable, States {
 
         return true;
     }
+
+    // withdraw 1% VTH tokens as fees by the Ventureum team
+    function withdrawVTHByVentureum() returns (bool) {
+
+        require( msg.sender != address(0x0) );  
+
+        // project must have been completed
+        require(projectMeta.completed());
+
+        // must be invoked by the Ventureum team 
+        require(msg.sender == _Ventureum.VentureumVTHAddr());
+
+        VTHOwnedByVentureum = 0;
+
+        ERC20 vth = ERC20(ven.getVTHAddr());
+        require(vth.transfer(msg.sender, VTHOwnedByVentureum));
+
+
+        return true;
+    }
+
+    // allow investors to transfer staked VTH tokens to another project's milestone
+    // the destination milestone must be in Ventureum Verified Milestone Contract Addresses
+    function transferVTH(address milestoneAddr, address remoteProjectMeta, uint value) returns (bool) {
+
+        require( msg.sender != address(0x0) );
+        
+        // project must have been completed
+        require(projectMeta.completed());
+
+        // the destination milestone must be in Ventureum Verified Milestone Contract Addresses
+        VentureumVerifiedMilestoneContractAddresses VVMCA = VentureumVerifiedMilestoneContractAddresses(_Ventureum.VVMCA());
+        require(VVMCA.isVerifiedMilestoneContractAddress(milestoneAddr)); //== true omitted
+
+        // the transferring amount of VTH must be less or equal to the amount staked
+        require(value <= staked[msg.sender]);
+
+        Milestone destMilestone = Milestone(milestoneAddr);
+
+        ProjectMeta RemoteProjectMeta = ProjectMeta(remoteProjectMeta);
+
+        ERC20 vth = ERC20(ven.getVTHAddr());
+
+        staked[msg.sender] -= value;
+
+        vth.approve(RemoteProjectMeta().VTHManagerAddr(), value);
+
+
+        return true;
+
+    }    
+
+
 }
