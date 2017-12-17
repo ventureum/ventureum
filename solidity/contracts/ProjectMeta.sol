7e2c47ccc32cc06e79d9b8d0d015caa5a2f0a9c9
Timothy Wang <timothywangdev@gmail.com>
2017-12-17 00:10:15 +0000

    Add ProjectMeta

---
+++ solidity/contracts/ProjectMeta.sol
@@ -0,0 +1,61 @@
+pragma solidity ^0.4.18;
+
+import './SafeMath.sol';
+import './Ownable.sol';
+import './States.sol';
+import './RefundManager.sol';
+import './ERC20.sol';
+import './Ballot.sol';
+import './Milestones.sol';
+
+contract ProjectMeta is Ownable, States {
+
+    // project name
+    string public name;
+
+    RefundManager public refundManager;
+
+    // milestones
+     Milestones public milestones;
+
+    Ballot public ballot;
+
+    // Class A project token contract
+    ERC20 public token;
+
+    // Class A token average crowdsale price in wei
+    uint public price;
+
+    function ProjectMeta(string _name) public {
+        name = _name;
+    }
+
+    function setRefundManager(address addr) external {
+        refundManager = RefundManager(addr);
+    }
+
+    
+    function setMilestones(address addr) external {
+        milestones = Milestones(addr);
+    }
+
+    /*
+    function setBallot(address addr) external {
+        ballot = Ballot(addr);
+    }
+
+    */
+
+    function setToken(address addr) external {
+        token = ERC20(addr);
+    }
+
+    function setTokenPrice(uint _price) external {
+        price = _price;
+    }
+
+    // token value in wei
+    function tokenToWei(uint tokens) external view returns (uint) {
+        return tokens * price;
+    }
+}
