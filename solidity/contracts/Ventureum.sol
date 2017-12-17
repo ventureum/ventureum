f2a1d57f8b43749bc78285f72fee698ce9e9a861
Timothy Wang <timothywangdev@gmail.com>
2017-12-17 00:13:25 +0000

    Add Ventureum contract

---
+++ solidity/contracts/Ventureum.sol
@@ -0,0 +1,30 @@
+pragma solidity ^0.4.18;
+
+import './Ownable.sol';
+import './ERC20.sol';
+
+// Ventureum project meta contract
+contract Ventureum is Ownable {
+
+    // VTH token contract address
+    ERC20 public VTHToken;
+
+    // number of mVTH to cover 1 wei
+    uint public exchangeRate;
+
+    function setExchangeRate(uint _exchangeRate) external {
+        exchangeRate = _exchangeRate;
+    }
+
+    /**
+     * @param value number of mVTH
+     * @return number of wei coveredtru
+     */
+    function mVTHToWei(uint value) external view returns (uint) {
+        return value / exchangeRate;
+    }
+
+    function setVTHToken(address addr) external {
+        VTHToken = ERC20(addr);
+    }
+}
