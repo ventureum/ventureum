27ae0a56f3a62c8379c9be094024c99295acefe3
Timothy Wang <timothywangdev@gmail.com>
2018-01-31 10:04:22 -0500

    Remove VP2, VP1_AFTER_VP2. Keep VP only

--- solidity/contracts/States.sol
+++ solidity/contracts/States.sol
@@ -4,9 +4,7 @@ contract States {
   // states
   uint8 constant INACTIVE = 0;
   uint8 constant IP = 1;
-  uint8 constant VP1 = 2;
-  uint8 constant VP2 = 3;
-  uint8 constant VP1_AFTER_VP2 = 4;
+  uint8 constant VP = 2;
   uint8 constant C = 5;
   uint8 constant RP = 6;
   uint8 constant TERMINAL = 7;
