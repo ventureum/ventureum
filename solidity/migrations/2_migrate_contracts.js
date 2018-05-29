72dbda8194465c268eed483c616a1d6fec3e82cf
b232wang <sky.stevenwang@gmail.com>
2018-05-29 15:26:54 -0400

    reorder variables (group by types)

--- solidity/migrations/2_migrate_contracts.js
+++ solidity/migrations/2_migrate_contracts.js
@@ -1,24 +1,43 @@
 var SafeMath = artifacts.require("./SafeMath.sol");
-var Base = artifacts.require("./kernel/Base.sol");
+
 var Kernel = artifacts.require("./kernel/Kernel.sol");
+var Base = artifacts.require("./kernel/Base.sol");
 var Module = artifacts.require("./kernel/Module.sol");
-var EtherCollector = artifacts.require("./collector/EtherCollector.sol");
-var EtherCollectorStorage = artifacts.require("./collector/EtherCollectorStorage.sol");
+
 var Handler = artifacts.require("./handlers/Handler.sol");
 var ACLHandler = artifacts.require("./handlers/ACLHandler.sol");
 var ContractAddressHandler = artifacts.require("./handlers/ContractAddressHandler.sol");
+
+var EtherCollector = artifacts.require("./collector/EtherCollector.sol");
+var EtherCollectorStorage = artifacts.require("./collector/EtherCollectorStorage.sol");
 var ProjectControllerModule = artifacts.require("./projectController/ProjectControllerModule.sol");
 
+const UNIT_DEPLOY_ADDRESS = "0xa0";
+
 module.exports = function(deployer) {
-    deployer.deploy(SafeMath);
-    deployer.link(SafeMath, EtherCollector);
-    deployer.deploy(Kernel);
-    deployer.deploy(Base,"0xa0");
-    deployer.deploy(Module, "0xa0");
-    deployer.deploy(EtherCollector, "0xa0");
-    deployer.deploy(EtherCollectorStorage, "0xa0");
-    deployer.deploy(Handler, "0xa0");
-    deployer.deploy(ACLHandler, "0xa0");
-    deployer.deploy(ContractAddressHandler, "0xa0");
-    deployer.deploy(ProjectControllerModule, "0xa0");
+    function unitDeploy() {
+        deployer.deploy(SafeMath);
+        deployer.link(SafeMath, EtherCollector);
+
+        deployer.deploy(Kernel);
+        deployer.deploy(Base, UNIT_DEPLOY_ADDRESS);
+        deployer.deploy(Module, UNIT_DEPLOY_ADDRESS);
+
+        deployer.deploy(Handler, UNIT_DEPLOY_ADDRESS);
+        deployer.deploy(ACLHandler, UNIT_DEPLOY_ADDRESS);
+        deployer.deploy(ContractAddressHandler, UNIT_DEPLOY_ADDRESS);
+
+        deployer.deploy(EtherCollector, UNIT_DEPLOY_ADDRESS);
+        deployer.deploy(EtherCollectorStorage, UNIT_DEPLOY_ADDRESS);
+        deployer.deploy(ProjectControllerModule, UNIT_DEPLOY_ADDRESS);
+    }
+    function testDeploy() {
+        deployer.deploy(SafeMath);
+        deployer.deploy(Kernel).then(() => {
+            return deployer.deploy(ProjectControllerModule, Kernel.address);
+        });
+        //TODO
+    }
+
+    unitDeploy();
 };
