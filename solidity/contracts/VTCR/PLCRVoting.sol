9affb0232103b688fd1221980d2148864946cbee
CHANGLONG ZHONG <vincentzhongca@gmail.com>
2018-06-01 14:13:15 -0400

    Bump solidity version to 0.4.24

--- solidity/contracts/VTCR/PLCRVoting.sol
+++ solidity/contracts/VTCR/PLCRVoting.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.23;
+pragma solidity ^0.4.24;
 
 import "vetx-token/contracts/VetXToken.sol";
 import "./DLL.sol";
--- solidity/contracts/kernel/IKernel.sol
+++ solidity/contracts/kernel/IKernel.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.23;
+pragma solidity ^0.4.24;
 
 interface IKernel {
     function handlers(bytes32 CI) external returns (address);
