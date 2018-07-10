7d9dbbe0bc6254e047641410c6a3d2e31e17d05b
b232wang <sky.stevenwang@gmail.com>
2018-07-10 14:31:40 -0400

    Fix #265 optimize mock data

--- solidity/script/vtcrWhitelist.js
+++ solidity/script/vtcrWhitelist.js
@@ -1,8 +1,12 @@
 const main = require("./mainScript.js")
-
-VTCR_WHITELIST = 1
-MILESTONE_NULL_ID = 0
+const mockData = require("./mockData.js")
 
 module.exports = async function (callback) {
-  await main(VTCR_WHITELIST, MILESTONE_NULL_ID, artifacts, web3.eth.accounts)
+  const data = mockData(artifacts)
+
+  await main(
+    data.STATES.VTCR_WHITELIST,
+    data.STATES.DEFAULT_MILESTONE_ID,
+    artifacts,
+    web3.eth.accounts)
 }
