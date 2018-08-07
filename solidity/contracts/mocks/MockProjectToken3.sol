02af5af4709b95cd971e0847a3a1f50d3f4ce305
b232wang <sky.stevenwang@gmail.com>
2018-08-07 13:03:46 -0400

    Fix #323 Add 4 Mock project tokens contract

---
+++ solidity/contracts/mocks/MockProjectToken3.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.4.24;
+
+import "openzeppelin-solidity/contracts/token/ERC20/StandardToken.sol";
+
+
+contract MockProjectToken3 is StandardToken {
+    string public constant name = "MockProjectToken3"; // solium-disable-line uppercase
+    string public constant symbol = "MPT"; // solium-disable-line uppercase
+    uint8 public constant decimals = 18; // solium-disable-line uppercase
+
+    uint256 public constant INITIAL_SUPPLY = 10000 * (10 ** uint256(decimals));
+
+    /**
+     * @dev Constructor that gives msg.sender all of existing tokens.
+     */
+    constructor() public {
+        totalSupply_ = INITIAL_SUPPLY;
+        balances[msg.sender] = INITIAL_SUPPLY;
+        emit Transfer(0x0, msg.sender, INITIAL_SUPPLY);
+    }
+}
