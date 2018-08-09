06a317c65de2620eb769162941e9b2acf9717b22
b232wang <sky.stevenwang@gmail.com>
2018-08-09 12:10:31 -0400

    Fix #331 migrate contracts to rinkeby test-net
    - update hd-wallet-provider to 0.0.6
    - remove `3_deploy_library.js`
    - using `mnemonic` with `hd-wallet-provider`
    - migrate all contracts to rinkeby

--- solidity/config/thirdPartyJsConfig.js
+++ solidity/config/thirdPartyJsConfig.js
@@ -1,11 +1,21 @@
+const rootDir = __dirname + '/../'
+
 import { expect } from 'chai'
 import {soliditySHA3} from 'ethereumjs-abi'
 
+const env = require(rootDir + '.env.json')
+
+
 export default function () {
   const fs = require('fs')
+  const HDWalletProvider = require('truffle-hdwallet-provider')
   const Web3 = require('web3')
-  const wweb3 = new Web3(
-    new Web3.providers.HttpProvider('http://localhost:8545'))
+  const ganachiWeb3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'))
+  const mnemonic = fs.readFileSync(rootDir + 'mnemonic.txt').toString().split('\n')[0]
+  let wweb3 = new Web3(new HDWalletProvider(mnemonic, 'http://localhost:8545'))
+  if (env['provider'] != "on") {
+    wweb3 = ganachiWeb3
+  }
   const BigNumber = wweb3.BigNumber
   const should = require('chai')
     .use(require('chai-as-promised'))
@@ -19,6 +29,7 @@ export default function () {
 
   return {
     'wweb3': wweb3,
+    'ganachiWeb3': ganachiWeb3,
     'Web3': Web3,
     'should': should,
     'fs': fs,
--- solidity/truffle.js
+++ solidity/truffle.js
@@ -21,8 +21,8 @@ module.exports = {
       host: '127.0.0.1',
       port: 8545,
       network_id: '*', // Match any network id
-      gas: 6000000
-      // provider: new HDWalletProvider(mnemonic, "http://localhost:8545",0,5) // <-- Comment this line when using solidity-coverage
+      gas: 6000000,
+      provider: new HDWalletProvider(mnemonic, "http://localhost:8545", 0, 10) // <-- Comment this line when using solidity-coverage
     },
     testing: {
       host: 'localhost',
