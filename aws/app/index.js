2a62378a1046795d160a1d6d11d2cec917f38883
b232wang <sky.stevenwang@gmail.com>
2018-08-09 20:13:30 -0400

    ventureum-backend aws optimization and full tested

--- aws/app/index.js
+++ aws/app/index.js
@@ -3,8 +3,8 @@ let loadInfo = require('./loader.js')
 
 const NAMESPACE = 'namespace'
 const NAME = 'name'
-const GET_VOTES = 'getVotes'
-const GET_GAS = 'getGas'
+const WRITE_AVAILABLE_VOTES= 'writeAvailableVotes'
+const GET_ESTIMATE_GAS= 'getEstimateGas'
 const POLL_ID = 'pollId'
 const ADDRESS = 'address'
 
@@ -17,10 +17,11 @@ var main = async function (event, context, callback) {
   info[NAMESPACE] = event[NAMESPACE]
   let CarbonVoteXSystem = new CarbonVoteX(info)
 
-  if (event[NAME] === GET_VOTES) {
-    CarbonVoteXSystem.getVotes(event[POLL_ID], event[ADDRESS], callback)
-  }
-  if (event[NAME] === GET_GAS) {
+  if (event[NAME] === WRITE_AVAILABLE_VOTES) {
+    CarbonVoteXSystem.writeAvailableVotes(event[POLL_ID], event[ADDRESS], callback)
+  } else if (event[NAME] === GET_ESTIMATE_GAS) {
     CarbonVoteXSystem.getEstimateGas(event[POLL_ID], event[ADDRESS], callback)
+  } else {
+    callback("The 'name' must be one of ['writeAvailableVotes', 'getEstimateGas']")
   }
 }
