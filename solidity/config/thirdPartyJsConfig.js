9a0ecab70758b6072894e5ef4e7af27a04f834d5
b232wang <sky.stevenwang@gmail.com>
2018-08-15 18:08:08 -0400

    Fix #352 thirdPartyJs read network info from truffle.js

--- solidity/config/thirdPartyJsConfig.js
+++ solidity/config/thirdPartyJsConfig.js
@@ -5,16 +5,23 @@ import {soliditySHA3} from 'ethereumjs-abi'
 
 const env = require(rootDir + '.env.json')
 
+const truffleInfo = require(rootDir + 'truffle.js')
+
 
 export default function () {
+
+  const HOST = truffleInfo.networks.development.host
+  const PORT = truffleInfo.networks.development.port
+  const provider = "http://" + HOST + ":" + PORT
+
   const fs = require('fs')
   const HDWalletProvider = require('truffle-hdwallet-provider')
   const Web3 = require('web3')
-  const ganachiWeb3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'))
+  const ganachiWeb3 = new Web3(new Web3.providers.HttpProvider(provider))
   const mnemonic = fs.readFileSync(rootDir + 'mnemonic.txt').toString().split('\n')[0]
   let wweb3 = ganachiWeb3
   if (env['provider'] == "on") {
-    wweb3 = new Web3(new HDWalletProvider(mnemonic, 'http://localhost:8545'))
+    wweb3 = new Web3(new HDWalletProvider(mnemonic, provider))
   }
   const BigNumber = wweb3.BigNumber
   const should = require('chai')
