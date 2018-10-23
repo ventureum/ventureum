b1b92cd6e88777e7d3b91b19c9ac1927bb1a7394
Timothy Wang <timothywangdev@gmail.com>
2018-10-23 18:48:55 +0000

    Resolve #400 Provide functionality to unregister users

--- v2_proto/contracts/RepSys.sol
+++ v2_proto/contracts/RepSys.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.23;
+pragma solidity ^0.4.24;
 
 import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 
@@ -7,7 +7,6 @@ contract RepSys {
     using SafeMath for uint;
 
     struct Profile {
-        bool init;
         bytes4 userType;
         uint reputation;
     }
@@ -17,7 +16,7 @@ contract RepSys {
         uint receivedVotes;
         uint usedVotesPct;
         address[] proxyList;
-        mapping(address => uint) votesCasted; // votes casted for each proxy
+        mapping(address => uint) votesPctCasted; // votes pct casted for each proxy
     }
 
     mapping(bytes32 => mapping(address => Delegation)) delegation;
@@ -32,39 +31,53 @@ contract RepSys {
         _;
     }
 
-    constructor() public {
+    modifier registered(address user) {
+        require(profile[user].userType != bytes4(0x0));
+        _;
+    }
+
+    constructor () public {
         owner = msg.sender;
     }
 
+    function calVotes(uint votes, uint pct) internal returns (uint) {
+        return (votes.mul(pct)).div(100);
+    }
+
+    /**
+     * Register/unregister a user
+     * @param user address of a user
+     * @param userType type of a user, set to 0 to unregister the user
+     * @param reputation init value of reputation
+     */
     function registerUser(address user, bytes4 userType, uint reputation) external onlyOwner {
         Profile storage p = profile[user];
 
-        p.init = true;
         p.userType = userType;
         p.reputation = reputation;
     }
 
-    function getProfile(address user) external view returns (bool, bytes4, uint) {
+    function getProfile(address user) public view returns (bytes4, uint) {
         Profile storage p = profile[user];
-        return (p.init, p.userType, p.reputation);
+        return (p.userType, p.reputation);
     }
 
-    function getWeight(address user) external view returns (uint) {
+    function getWeight(address user) public view returns (uint) {
         return profile[user].reputation;
     }
 
-    function getDelegation(bytes32 projectId, address user) external view returns (uint, uint ,uint) {
+    function getDelegation(bytes32 projectId, address user) public view returns (uint, uint ,uint) {
         Delegation storage d = delegation[projectId][user];
         return (d.votes, d.receivedVotes, d.usedVotesPct);
     }
 
-    function updateDelegation(bytes32 projectId, address principal, address proxy, uint pos, uint neg) public {
+    function updateDelegation(bytes32 projectId, address principal, address proxy, uint pos, uint neg) internal {
         Delegation storage d = delegation[projectId][proxy];
 
         d.receivedVotes = d.receivedVotes.add(pos).sub(neg);
     }
 
-    function writeVotes(bytes32 projectId, address user, uint val) external {
+    function writeVotes(bytes32 projectId, address user, uint val) external onlyOwner registered(user) {
         Delegation storage d = delegation[projectId][user];
         uint pos = 0;
         uint neg = 0;
@@ -86,8 +99,8 @@ contract RepSys {
                 projectId,
                 user,
                 proxy,
-                pos.mul(d.votesCasted[proxy]).div(100),
-                neg.mul(d.votesCasted[proxy]).div(100));
+                pos.mul(d.votesPctCasted[proxy]).div(100),
+                neg.mul(d.votesPctCasted[proxy]).div(100));
         }
     }
 
@@ -95,12 +108,12 @@ contract RepSys {
         require(0 <= pct && pct <= 100, "Invalid percentage");
         Delegation storage d = delegation[projectId][msg.sender];
 
-        if (d.votesCasted[proxy] == 0 && pct > 0) {
+        if (d.votesPctCasted[proxy] == 0 && pct > 0) {
             // new proxy added to the list
             d.proxyList.push(proxy);
         }
 
-        if (d.votesCasted[proxy] > 0 && pct == 0) {
+        if (d.votesPctCasted[proxy] > 0 && pct == 0) {
             uint target = 0;
             // remove proxy from the list
             for(uint i = 0; i < d.proxyList.length; i++) {
@@ -113,18 +126,18 @@ contract RepSys {
             d.proxyList.length--;
         }
 
-        if (pct >= d.votesCasted[proxy]) {
-            require (d.usedVotesPct.add(pct.sub(d.votesCasted[proxy])) <= 100, "Cannot exceed 100%");
-            d.usedVotesPct = d.usedVotesPct.add(pct.sub(d.votesCasted[proxy]));
+        if (pct >= d.votesPctCasted[proxy]) {
+            require (d.usedVotesPct.add(pct.sub(d.votesPctCasted[proxy])) <= 100, "Cannot exceed 100%");
+            d.usedVotesPct = d.usedVotesPct.add(pct.sub(d.votesPctCasted[proxy]));
 
             // update proxy's votes
-            updateDelegation(projectId, msg.sender, proxy, d.votes.mul(pct.sub(d.votesCasted[proxy])), 0);
+            updateDelegation(projectId, msg.sender, proxy, calVotes(d.votes, pct.sub(d.votesPctCasted[proxy])), 0);
         } else {
             // require is not required since we have safemath
-            d.usedVotesPct = d.usedVotesPct.sub(d.votesCasted[proxy].sub(pct));
-            updateDelegation(projectId, msg.sender, proxy, 0, d.votes.mul(d.votesCasted[proxy].sub(pct)));
+            d.usedVotesPct = d.usedVotesPct.sub(d.votesPctCasted[proxy].sub(pct));
+            updateDelegation(projectId, msg.sender, proxy, 0, calVotes(d.votes, d.votesPctCasted[proxy].sub(pct)));
         }
 
-        d.votesCasted[proxy] = pct;
+        d.votesPctCasted[proxy] = pct;
     }
 }
