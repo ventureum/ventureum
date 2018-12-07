a7ccbe33634c678e7275f81e115b6e2a5788ddfc
Timothy Wang <timothywangdev@gmail.com>
2018-12-07 00:16:17 +0000

    Fix #483 Incorrect container name for listener

--- v2_proto/docker/listener/run.sh
+++ v2_proto/docker/listener/run.sh
@@ -1,2 +1,2 @@
 #/bin/bash
-sudo docker run -d --restart unless-stopped --name loom --net=host -v ~/ventureum:/ventureum ventureum/listener:latest
+sudo docker run -d --restart unless-stopped --name listener --net=host -v ~/ventureum:/ventureum ventureum/listener:latest
